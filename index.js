be867f6b3a973158c5020f7182a70f40a23a5db9
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-04-15 23:08:40 +0530

    Update

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ MongoClient.connect(url).then((client) => {
 
     const db = client.db(dbname);
 
-    dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes')
+    dboper.insertDocument(db, {name: "Vada and Idli", description: 'Test'}, 'dishes')
     .then((result) => {
         console.log('Insert Document: \n', result.ops);
         return dboper.findDocuments(db, 'dishes')
     })
     .then((docs) => {
-        console.log('FOund Documents: \n', docs);
-        return dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated Test'}, 'dishes')
+        console.log('Found Documents: \n', docs);
+        return dboper.updateDocument(db, {name: 'Vada and Idli'}, {description: 'Updated Test'}, 'dishes')
     })
     .then((result) => {
         console.log('Updated Document : \n', result.result);
